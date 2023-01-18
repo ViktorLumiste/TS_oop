00bc6f28a0c2875fee76fae8a396ec18a6d77eec
ViktorLumiste <viktor.lumiste@gmail.com>
2023-01-18 12:32:52 +0200

    Ül ts-6 lahendus

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,6 +1,12 @@
-let resistors:number[]=[4,2];
+let voltage:number = 4.5
+let amps:number = 0.5
+let ohms:number = voltage / amps
+let power:number = voltage * amps
+let resistors:number[]=[110, 220, 220];
 let inversesum:number=0;
 for(let resistance of resistors){
     inversesum+=1/resistance;
 }
 console.log(1/inversesum);
+console.log(ohms)
+console.log(power)
