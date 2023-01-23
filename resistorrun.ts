cb870206a754a9b359b3cdf1b542c3470fb87ea1
ViktorLumiste <viktor.lumiste@gmail.com>
2023-01-23 14:35:26 +0200

    Ül ts-8 lahendus

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,91 +1,59 @@
 class Resistor {
     r: number = 0;
-    constructor(r: number) {
+    name: String;
+    constructor(r: number, name: String) {
         this.r = r;
+        this.name = name;
     }
     getResistance(): number {
         return this.r;
     }
-}
-
-class Switch extends Resistor {
-    constructor() {
-        super(0);
+    getCurrent(u: number): number {
+        return u / this.getResistance();
     }
-    on() {
-        this.r = 0;
+    getDescription() {
+        return "takisti " + this.name ;
     }
-    off() {
-        this.r = 1000000000;
+    getObject() {
+        return "takisti " + this.name + " " + this.getResistance() + " ohm";
     }
 }
 
-class SeriesCircuit {
+
+class SeriesCircuit extends Resistor {
     resistors: Resistor[] = []
+    constructor(name: String) {
+        super(0, name);
+    }
     push(r: Resistor) {
         this.resistors.push(r);
     }
-    getTotalResistance() {
+    getResistance() {
         let sum: number = 0;
         this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
         return sum;
     }
-    getCurrent(u: number) {
-        return u / this.getTotalResistance();
+    getDescription(): string {
+        return "SeriesCircuit " + this.name + "(" + this.resistors.map(r => r.getDescription()).join(", ") + ")";
     }
-    getPower(u:number){
-        return u* this.getCurrent(u)
+    getObjects():string{
+        return "Jada " + this.name + " " + this.getResistance()+ " ohm"+ "(" + this.resistors.map(r => r.getObject()).join(", ") + ")";
     }
 }
-class ParallelCircuit{
-    resistors: Resistor[] = []
-    push(r: Resistor) {
-        this.resistors.push(r);
-    }
-    getTotalResistance(){
-        let sum:number = 0
-        this.resistors.forEach((r:Resistor) => {
-            if(r.r > 0) {
-                sum += 1 / r.r
-            }
-        })
-        return sum
-    }
 
-}
-
-// let r1: Resistor = new Resistor(220);
-// let sw1: Switch = new Switch();
-// let sw2: Switch = new Switch();
-// let sc1: SeriesCircuit = new SeriesCircuit();
-// sc1.push(r1);
-// sc1.push(sw1);
-// sc1.push(sw2);
-// sw1.off()
-// sw2.off()
-// console.log(sc1.getTotalResistance());
-// console.log(sc1.getCurrent(5));
-// console.log(sc1.getPower(5));
-// sw1.on();
-// console.log(sc1.getTotalResistance());
-// console.log(sc1.getCurrent(5));
-// console.log(sc1.getPower(5));
-// sw2.on()
-// console.log(sc1.getTotalResistance());
-// console.log(sc1.getCurrent(5));
-// console.log(sc1.getPower(5));
-
-let r1:Resistor = new Resistor(220)
-let sw1:Switch = new Switch()
-let sw2:Switch = new Switch()
-let pc1:ParallelCircuit = new ParallelCircuit()
-pc1.push(r1)
-pc1.push(sw1)
-pc1.push(sw2)
-sw1.off()
-sw2.off()
-console.log(pc1.getTotalResistance())
-sw2.on()
-console.log(pc1.getTotalResistance())
-sw1.on()
-console.log(pc1.getTotalResistance())
+let r1: Resistor = new Resistor(100, "t1");
+let r2: Resistor = new Resistor(150, "t2");
+let r3: Resistor = new Resistor(175, "t3");
+let r4: Resistor = new Resistor(10, "t4");
+let sc1: SeriesCircuit = new SeriesCircuit("j1");
+let sc2: SeriesCircuit = new SeriesCircuit("j2");
+let sc3: SeriesCircuit = new SeriesCircuit("j3");
+sc3.push(r1);
+sc3.push(r2);
+sc2.push(r3);
+sc1.push(r4);
+sc1.push(sc2);
+sc2.push(sc3);
+console.log(sc1.getObjects());
+console.log(sc2.getObjects());
+console.log(sc3.getObjects());
